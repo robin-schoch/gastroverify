59435e94ecd260defe5221cc6a3ecf7a2c2633bd
Robin Schoch <rschoch1995@gmail.com>
2020-08-04 08:11:39 +0200

    init commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import API from '@aws-amplify/api';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+import awsconfig from './aws-exports';
 if (environment.production) {
   enableProdMode();
 }
+
+
+
+API.configure(awsconfig);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
