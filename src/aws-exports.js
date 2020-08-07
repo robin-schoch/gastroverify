487b12778388e891d3576105a6aca6315fbbd3bb
Robin Schoch <rschoch1995@gmail.com>
2020-08-07 23:04:11 +0200

    please

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -8,13 +8,6 @@ const awsmobile = {
             "name": "v1",
             "endpoint": "https://5cyg79qk1a.execute-api.eu-central-1.amazonaws.com/dev",
             "region": "eu-central-1",
-            custom_header: async () => {
-                return {Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}`}
-                // Alternatively, with Cognito User Pools use this:
-                // return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
-                // return { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` }
-            }
-
         }
     ],
     "aws_dynamodb_all_tables_region": "eu-central-1",
