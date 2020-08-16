dea92c8707adf97caaf94cb2fb74cc3326a313a7
Robin Schoch <rschoch1995@gmail.com>
2020-08-16 21:08:59 +0200

    secret manager

--- amplify/backend/function/visitors/src/util/jwtUtil.js
+++ amplify/backend/function/visitors/src/util/jwtUtil.js
@@ -1,5 +1,39 @@
 const jwt = require('jsonwebtoken');
-const secret = "notSAFEEEEE!!!" // where should it be??
+
+var AWS = require('aws-sdk'),
+    region = "eu-central-1",
+    secretName = "JWT_SECRET",
+    decodedBinarySecret;
+
+let secret
+var client = new AWS.SecretsManager({
+    region: region
+});
+let env = 'dev'
+console.log("env = == = + " + env)
+if (process.env.ENV && process.env.ENV !== "NONE") {
+    env = process.env.ENV;
+}
+const loadSecret = () => {
+    return new Promise((resolve, reject) => {
+        client.getSecretValue({SecretId: secretName}, (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(resolve(JSON.parse(data.SecretString)[env]))
+            }
+        })
+    })
+}
+
+const getSecret =   () => {
+    if (!secret) {
+        console.log("loading secret")
+        loadSecret().then(sec => secret = sec)
+    }
+}
+
+getSecret()
 
 module.exports.generateJWT = (phoneNumber, momentum) => {
     return jwt.sign({
