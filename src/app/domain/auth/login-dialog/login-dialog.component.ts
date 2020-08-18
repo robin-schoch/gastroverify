60ec148e979e4915baf78173ca5c4e5b85deeb8f
Robin Schoch <rschoch1995@gmail.com>
2020-08-18 10:05:22 +0200

    test

---
+++ src/app/domain/auth/login-dialog/login-dialog.component.ts
@@ -0,0 +1,55 @@
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+import {FormFieldTypes, onAuthUIStateChange} from '@aws-amplify/ui-components';
+import {CognitoUser} from 'amazon-cognito-identity-js';
+import {AuthenticationService} from '../authentication.service';
+
+@Component({
+    selector: 'app-login-dialog',
+    templateUrl: './login-dialog.component.html',
+    styleUrls: ['./login-dialog.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginDialogComponent implements OnInit, OnDestroy {
+
+    formFields: FormFieldTypes;
+
+    constructor(
+        public dialogRef: MatDialogRef<LoginDialogComponent>,
+        private authService: AuthenticationService,
+        private ref: ChangeDetectorRef
+    ) {
+        this.formFields = [
+            {
+                type: 'email',
+                label: 'Email',
+                placeholder: 'Email',
+                required: true,
+            },
+            {
+                type: 'password',
+                label: 'Password',
+                placeholder: 'Password',
+                required: true,
+            },
+        ];
+    }
+
+    ngOnInit(): void {
+        onAuthUIStateChange((authState, authData) => {
+
+            if (authState === 'signedin') {
+                this.dialogRef.close();
+                this.authService.isAuthenticated = true;
+                this.authService.activeUser = authData as CognitoUser;
+            }
+            this.ref.detectChanges();
+        });
+    }
+
+    ngOnDestroy() {
+        return onAuthUIStateChange;
+    }
+
+
+}
