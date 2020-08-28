7c4667f78c353562054ff4444b84253d888f6ec5
Robin Schoch <rschoch1995@gmail.com>
2020-08-28 12:03:12 +0200

    fixed error when creating new user

--- src/app/domain/auth/sign-out.directive.ts
+++ src/app/domain/auth/sign-out.directive.ts
@@ -1,19 +1,25 @@
 import {Directive, HostListener} from '@angular/core';
 import {AuthenticationService} from './authentication.service';
 import {Router} from '@angular/router';
+import {GastroService} from '../gastro-dashboard/gastro.service';
 
 @Directive({
     selector: '[appSignOut]'
 })
 export class SignOutDirective {
 
-    constructor(private authService: AuthenticationService, private router: Router) { }
+    constructor(
+        private authService: AuthenticationService,
+        private router: Router,
+        private locationService: GastroService
+    ) { }
 
     @HostListener(
         'click',
         ['$event']
     )
     onClick(e) {
+        this.locationService.gastro = null;
         this.authService.signOut();
         this.authService.role = [];
     }
