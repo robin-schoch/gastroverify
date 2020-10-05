bd76d280c77387bca615e0a895081280c6c14849
Robin Schoch <rschoch1995@gmail.com>
2020-10-05 14:51:59 +0200

    fuck it

--- amplify/backend/function/dailyReport/src/index.js
+++ amplify/backend/function/dailyReport/src/index.js
@@ -55,7 +55,7 @@ const createReportForLocation = async (date, location) => {
     });
 };
 exports.handler = async (event) => {
-    const creationTime = moment().hours(6).minutes(0).seconds(0).milliseconds(0);
+    const creationTime = moment().hours(8).minutes(0).seconds(0).milliseconds(0);
     log.info('create reports: ' + creationTime.toISOString());
     const dat = creationTime.clone();
     let lastEvaluatedPartnerKey = null;
--- amplify/backend/function/sendBill/src/index.js
+++ amplify/backend/function/sendBill/src/index.js
@@ -1,19 +1,20 @@
+"use strict";
 /* Amplify Params - DO NOT EDIT
-	ENV
-	REGION
-	STORAGE_MONTHLYREPORT_ARN
-	STORAGE_MONTHLYREPORT_NAME
-Amplify Params - DO NOT EDIT */
-
+ ENV
+ REGION
+ STORAGE_MONTHLYREPORT_ARN
+ STORAGE_MONTHLYREPORT_NAME
+ Amplify Params - DO NOT EDIT */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
 exports.handler = async (event) => {
-    // TODO implement
     const response = {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+        //  Uncomment below to enable CORS requests
+        //  headers: {
+        //      "Access-Control-Allow-Origin": "*"
+        //  },
+        body: JSON.stringify(event),
     };
     return response;
 };
