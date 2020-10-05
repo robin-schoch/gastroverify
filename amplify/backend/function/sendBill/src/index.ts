9cf26433af82a4f517e0ae8e00421891184cf027
Robin Schoch <rschoch1995@gmail.com>
2020-10-05 12:46:44 +0200

    added new function for emails

---
+++ amplify/backend/function/sendBill/src/index.ts
@@ -0,0 +1,19 @@
+/* Amplify Params - DO NOT EDIT
+ ENV
+ REGION
+ STORAGE_MONTHLYREPORT_ARN
+ STORAGE_MONTHLYREPORT_NAME
+ Amplify Params - DO NOT EDIT */
+
+
+export const handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*"
+    //  },
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+}
