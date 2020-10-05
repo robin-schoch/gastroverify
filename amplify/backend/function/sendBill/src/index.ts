14af964cf46601a3bf965191062d9e16a0543523
Robin Schoch <rschoch1995@gmail.com>
2020-10-05 18:11:48 +0200

    added a lyer

--- amplify/backend/function/sendBill/src/index.ts
+++ amplify/backend/function/sendBill/src/index.ts
@@ -7,6 +7,9 @@
 
 
 
+
+
+
 export const handler = async (event) => {
   const response = {
     statusCode: 200,
---
+++ amplify/backend/function/utility/opt/test.ts
@@ -0,0 +1,3 @@
+export const iamTheLayer = () => {
+  console.log('hello from the ohter side!');
+};
